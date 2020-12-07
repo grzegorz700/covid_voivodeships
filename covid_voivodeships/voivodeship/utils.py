b8c1b52b31885dbd6ee09735182f02053d3ce8f4
grzegorz700 <grzegorz7w@gmai.com>
2020-12-07 17:08:17 +0100

    append missing data

--- covid_voivodeships/voivodeship/utils.py
+++ covid_voivodeships/voivodeship/utils.py
@@ -6,7 +6,7 @@ from covid_voivodeships.storage.structure import ProjectDataStructure
 
 def _load_links_with_names(root_dir='../..'):
     ps = ProjectDataStructure()
-    pth = os.path.abspath(os.path.join(ps.data_dir, 'voivodeship_links.txt'))
+    pth = os.path.abspath(os.path.join(ps.data_dir, 'essential', 'voivodeship_links.txt'))
     with open(pth) as f:
         voivodeship_links = f.readlines()
     voivodeship_links = [vl.strip() for vl in voivodeship_links]
